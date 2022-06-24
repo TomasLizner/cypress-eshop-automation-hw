b66cd2f0cfa2d20ad5630212c314b25b4f53bc6b
TOM-PC\Tom-PC <t.lizner@seznam.cz>
2022-06-24 19:09:19 +0200

    Add Test Case for inserting 2 TVs into the basket

--- cypress/e2e/cypress-eshop-automation-hw.cy.js
+++ cypress/e2e/cypress-eshop-automation-hw.cy.js
@@ -1,5 +1,7 @@
 describe("Add two most expensive TVs to the shopping cart.", () => {
 
+  const PRODUCT = "Televize"
+
   beforeEach(()=>{
     cy.visit("https://www.mironet.cz/")
     cy.get('.col-text > .ButtonDef').click()
@@ -10,5 +12,14 @@ describe("Add two most expensive TVs to the shopping cart.", () => {
     cy.get('[id=EXPS]').should('be.visible')
     cy.get('.BasketSumInfo').should('be.visible')
     cy.get('[id=div_footX').should('be.visible')
+  }),
+  it.only("navigates to TVs category and adds two most expensive TVs to the shopping cart", () => {
+    cy.get('[id=EXPS]').type(`${PRODUCT}{enter}`)
+    cy.get('.additional_search_category > span').contains(`${PRODUCT}`).click()
+    cy.get('[id=detail-tabs]').contains('Nejdražší').click()
+    cy.get('[id=productContainer] input[type=submit').eq(0).click()
+    cy.get('.kosik_zpet').eq(0).click()
+    cy.get('[id=productContainer] input[type=submit').eq(1).click()
+    cy.get('[id=produkty]').find('.tr_kosik_produkt').should('have.length', 2)
   })
 })
