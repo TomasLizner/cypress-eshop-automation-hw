f43990a1f6726aa36eb3f8f0007bfaf7c6ee0f64
TOM-PC\Tom-PC <t.lizner@seznam.cz>
2022-06-26 11:13:28 +0200

    Move repeatable actions to separate function and constants

--- cypress/e2e/cypress-eshop-automation-hw.cy.js
+++ cypress/e2e/cypress-eshop-automation-hw.cy.js
@@ -1,6 +1,7 @@
 describe("Add two most expensive TVs to the shopping cart.", () => {
 
   const PRODUCT = "Televize"
+  const SEARCH_FIELD_ID = "EXPS"
 
   beforeEach(()=>{
     cy.visit("https://www.mironet.cz/")
@@ -9,17 +10,18 @@ describe("Add two most expensive TVs to the shopping cart.", () => {
 
   it("opens homepage and verifies if expected objects are loaded properly", () => {
     cy.get('[id=shp_logo]').should('be.visible')
-    cy.get('[id=EXPS]').should('be.visible')
+    cy.get(`[id=${SEARCH_FIELD_ID}]`).should('be.visible')
     cy.get('.BasketSumInfo').should('be.visible')
     cy.get('[id=div_footX').should('be.visible')
   }),
-  it.only("navigates to TVs category and adds two most expensive TVs to the shopping cart", () => {
-    cy.get('[id=EXPS]').type(`${PRODUCT}{enter}`)
+
+  it("navigates to TVs category and adds two most expensive TVs to the shopping cart", () => {
+    cy.get(`[id=${SEARCH_FIELD_ID}]`).type(`${PRODUCT}{enter}`)
     cy.get('.additional_search_category > span').contains(`${PRODUCT}`).click()
     cy.get('[id=detail-tabs]').contains('Nejdražší').click()
-    cy.get('[id=productContainer] input[type=submit').eq(0).click()
-    cy.get('.kosik_zpet').eq(0).click()
-    cy.get('[id=productContainer] input[type=submit').eq(1).click()
+    cy.clickOnAddToCartButton(0)
+    cy.get('.kosik_zpet:eq(0)').click()
+    cy.clickOnAddToCartButton(1)
     cy.get('[id=produkty]').find('.tr_kosik_produkt').should('have.length', 2)
   })
 })
---
+++ cypress/support/commands.js
@@ -0,0 +1,3 @@
+Cypress.Commands.add("clickOnAddToCartButton", (buttonOrder) => { 
+    cy.get('[id=productContainer] input[type=submit').eq(`${buttonOrder}`).click()
+  })
